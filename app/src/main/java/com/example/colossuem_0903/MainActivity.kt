22b54c6a27ed14ab04199730457b17665916201b
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-03 17:07:44 +0900

    서버 응답 code 추출 연습

--- app/src/main/java/com/example/colossuem_0903/MainActivity.kt
+++ app/src/main/java/com/example/colossuem_0903/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.colossuem_0903
 
 import android.os.Bundle
+import com.example.colossuem_0903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -16,8 +17,11 @@ class MainActivity : BaseActivity() {
 
         signIn_Btn.setOnClickListener {
 
-            val inputId = email_Edt.text
-            val inputPw = password_Edt.text
+            val inputId = email_Edt.text.toString()
+            val inputPw = password_Edt.text.toString()
+
+//            서버에 이 데이터가 회원이 맞는지? 확인 요청. => 로그인 시도.
+            ServerUtil.postRequestSignIn(inputId, inputPw)
         }
     }
 
--- app/src/main/java/com/example/colossuem_0903/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colossuem_0903/utils/ServerUtil.kt
@@ -1,8 +1,9 @@
 package com.example.colossuem_0903.utils
 
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class ServerUtil {
 
@@ -42,8 +43,35 @@ class ServerUtil {
 //            요청을 한다 -> 클라이언트의 역할. -> 앱이 클라이언트로 동작해야함.
             val client = OkHttpClient()
 
-//            만들어진 요청 호출.
-            client.newCall(request)
+//            만들어진 요청 호출. => 응답이 왔을때 분석 / UI 반영
+//            호출을 하면 -> 서버가 데이터 처리... -> 응답 받아서 처리. (처리할 코드를 등록)
+            client.newCall(request).enqueue(object : Callback {
+
+                override fun onFailure(call: Call, e: IOException) {
+
+//                    실패? 서버 연결 자체를 실패. 응답 X.
+//                    비번 틀려서 로그인 실패 : 응답은 돌아왔는데, 그 내용이 실패. (응답 O)
+//                    인터넷 끊김, 서버 접속 불가 등, 실패
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+//                    어떤 내용이든, 응답이 돌아온 경우. (로그인 성공, 실패 모두 응답)
+//                    응답에 포함된 데이터들 중 -> 본문(body) 을 보자.
+
+                    val bodyString = response.body!!.string()
+//                    본문을 그냥 String 으로 찍어보면 -> 한글이 깨져보임.
+//                    JSONObject 형태로 변환해서 -> 다시 String 으로 바꿔보면 한글이 보임.
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("서버응답본문", jsonObj.toString())
+
+//                    code 값 추출 연습. -> 화면에서 분석해서, 토스트를 띄우는 등의 UI 처리
+//                    val code = jsonObj.getInt("code")
+//
+//                    Log.d("코드 값", code.toString())
+                }
+            })
         }
     }
 }

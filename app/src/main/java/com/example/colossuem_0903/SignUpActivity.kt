9ebe1b10636eb65029cbeabab5c858159be3594b
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-06 10:47:57 +0900

    가입 실패시에 대한 사유 띄우기

--- app/src/main/java/com/example/colossuem_0903/SignUpActivity.kt
+++ app/src/main/java/com/example/colossuem_0903/SignUpActivity.kt
@@ -1,7 +1,10 @@
 package com.example.colossuem_0903
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.colossuem_0903.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -19,5 +22,41 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        signUp_Btn.setOnClickListener {
+//            1. 입력한 값 받아서
+            val inputEmail = email_Edt.text.toString()
+            val inputPw = pw_Edt.text.toString()
+            val inputNickName = nickName_Edt.text.toString()
+
+//            2. 서버에서 가입요청 -> 응답자에 따라 어떤 처리?
+            ServerUtil.putRequestSignUp(
+                inputEmail,
+                inputPw,
+                inputNickName,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObj: JSONObject) {
+
+//                        가입 성공 (200) / 실패 (200 외의 값)
+//                        실패: 이메일 양식 x, 중복된 이메일, 중복된 닉네임
+
+//                        성공일때 -> {} 내부 비워두자.
+//                        실패시만 동작 -> message 에 담긴 가입 실패 사유를 갖고 -> 토스트로 출력,
+
+                        val code = jsonObj.getInt("code")
+
+                        if (code == 200) {
+
+
+                        } else {
+                            val message = jsonObj.getString("message")
+
+                            runOnUiThread {
+
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                })
+        }
     }
 }
--- app/src/main/java/com/example/colossuem_0903/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colossuem_0903/utils/ServerUtil.kt
@@ -82,42 +82,46 @@ class ServerUtil {
                 }
             })
         }
+
+        //    회원가입 실행 함수.
+        fun putRequestSignUp(
+            email: String,
+            password: String,
+            nickname: String,
+            handler: JsonResponseHandler?
+        ) {
+
+            val urlString = "${HOST_URL}/user"
+
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", password)
+                .add("nick_name", nickname)
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .put(formData)
+                .build()
+
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답본문", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+                }
+            })
+        }
     }
 
-    //    회원가입 실행 함수.
-    fun putRequestSignUp(
-        email: String,
-        password: String,
-        nickname: String,
-        handler: JsonResponseHandler?
-    ) {
 
-        val urlString = "${HOST_URL}/user"
 
-        val formData = FormBody.Builder()
-            .add("email", email)
-            .add("password", password)
-            .add("nick_name", nickname)
-            .build()
 
-        val request = Request.Builder()
-            .url(urlString)
-            .put(formData)
-            .build()
-
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-
-                val bodyString = response.body!!.string()
-                val jsonObj = JSONObject(bodyString)
-                Log.d("서버응답본문", jsonObj.toString())
-                handler?.onResponse(jsonObj)
-            }
-        })
-    }
 }

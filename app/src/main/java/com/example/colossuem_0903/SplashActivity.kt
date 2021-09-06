3802ef7902938093b4289461f80b5b90c692d7a2
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-06 15:58:06 +0900

    SplashActivity.kt 추가 -> 시작화면으로 변경  - 테마도 노액션바로 설정  - 자동로그인 여부 판단, 어느 화면으로 이동할지? 코딩 예정

---
+++ app/src/main/java/com/example/colossuem_0903/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.colossuem_0903
+
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setValues()
+        setupEvents()
+    }
+
+    override fun setValues() {
+
+
+    }
+
+    override fun setupEvents() {
+
+
+    }
+}

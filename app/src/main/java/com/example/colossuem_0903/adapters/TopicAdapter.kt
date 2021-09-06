51bfd6e9e0d1c25376855ae6d437a2a7701c6dfa
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-06 15:06:27 +0900

    ListView Adapter 뼈대 작업

---
+++ app/src/main/java/com/example/colossuem_0903/adapters/TopicAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.colossuem_0903.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.colossuem_0903.R
+import com.example.colossuem_0903.datas.TopicData
+
+class TopicAdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: List<TopicData>
+) : ArrayAdapter<TopicData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        if (row == null) {
+            row = mInflater.inflate(R.layout.topic_list_item, null)
+        }
+        row!!
+        
+        return row
+    }
+}
---
+++ app/src/main/java/com/example/colossuem_0903/datas/TopicData.kt
@@ -0,0 +1,4 @@
+package com.example.colossuem_0903.datas
+
+class TopicData(var id : Int, var title: String, var imgURL: String) {
+}

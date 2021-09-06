11eb58e1a9590f012cbd76891fd75d2ce495ba13
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-06 15:36:49 +0900

    서버의 TopicList 에서 -> TopicData 목록으로 변환 작업중.  - for 문 사용  - TopicData 의 보조 생성자 추가.

--- app/src/main/java/com/example/colossuem_0903/MainActivity.kt
+++ app/src/main/java/com/example/colossuem_0903/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.colossuem_0903
 
 import android.os.Bundle
+import com.example.colossuem_0903.datas.TopicData
 import com.example.colossuem_0903.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<TopicData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,13 +26,27 @@ class MainActivity : BaseActivity() {
 
     }
 
-//    서버에서, 메인화면에 보여줄 정보 받아오기
+    //    서버에서, 메인화면에 보여줄 정보 받아오기
     private fun getMainDataFromServer() {
 
-        ServerUtil.getRequestMainData(mContext, object: ServerUtil.JsonResponseHandler {
+        ServerUtil.getRequestMainData(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
 
+//                응답 - jsonObj 분석 (파싱) => 토론 주제들을 서버가 내려줌.
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
 
+//                서버가 내려주는 토론 주제들 (JSONObject 목록) => TopicData 로 변환해서 ArrayList 에 추가. (반복문 활용)
+
+//                topicArr 에 4개 : 0 ~ 4 직전 까지 반복.
+                for (i in 0 until topicsArr.length()) {
+
+//                    순서에 맞는 {} 를 통째로 받아내기. => 토론 주제 하나하나에 대한 정보를 담게됨.
+                    val topicObj = topicsArr.getJSONObject(i)
+
+//                    TopicData 를 만들어서 -> 멤버변수들에 -> TopicObj 에서 파싱한 데이터를 대입.
+                    val tempTopicData = TopicData()
+                }
             }
         })
     }
--- app/src/main/java/com/example/colossuem_0903/datas/TopicData.kt
+++ app/src/main/java/com/example/colossuem_0903/datas/TopicData.kt
@@ -1,4 +1,10 @@
 package com.example.colossuem_0903.datas
 
-class TopicData(var id : Int, var title: String, var imgURL: String) {
+class TopicData(var id: Int, var title: String, var imgURL: String) {
+
+    //    보조 생성자 추가.
+    constructor() : this(0, "제목없음", "")
+
+    //    연습. id 값만 받는 보조 생성자
+    constructor(id: Int) : this(id, "제목없음", "")
 }

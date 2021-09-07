0da20010260219f5a4ffeabebce86a2b0e527e32
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-07 15:40:16 +0900

    게시글 작성 화면 UI 작업

---
+++ app/src/main/java/com/example/colossuem_0903/ReplyEditActivity.kt
@@ -0,0 +1,18 @@
+package com.example.colossuem_0903
+
+import android.os.Bundle
+
+class ReplyEditActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reply_edit)
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun setupEvents() {
+
+    }
+}

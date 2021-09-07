bd122883165dbb4316f5c6e876f6ec7723e86f0b
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-07 10:55:43 +0900

    댓글 표현 데이터 클래스 생성  - 멤버변수를 var 로  - 보조 생성자 () 형태 지원  - json 기반으로 -> ReplyData 객체 생성 기능

---
+++ app/src/main/java/com/example/colossuem_0903/datas/ReplyData.kt
@@ -0,0 +1,35 @@
+package com.example.colossuem_0903.datas
+
+import org.json.JSONObject
+
+class ReplyData(
+    var id: Int,
+    var content: String,
+    var likeCount: Int,
+    var disLikeCount: Int,
+    var myLike: Boolean,
+    var myDisLike: Boolean,
+    var replyCount: Int
+) {
+
+    constructor() : this(0, "", 0, 0, false, false, 0)
+
+    companion object {
+
+        //        JSON 을 넣으면 -> ReplyData 로 변환해주는 기능
+        fun getReplyDataFromJson(json: JSONObject): ReplyData {
+
+            val replyData = ReplyData()
+
+            replyData.id = json.getInt("id")
+            replyData.content = json.getString("content")
+            replyData.likeCount = json.getInt("like_count")
+            replyData.disLikeCount = json.getInt("dislike_count")
+            replyData.myLike = json.getBoolean("my_like")
+            replyData.myDisLike = json.getBoolean("my_dislike")
+            replyData.replyCount = json.getInt("reply_count")
+
+            return replyData
+        }
+    }
+}
